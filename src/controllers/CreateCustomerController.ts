8c6e62a148f482832d592746de96cf27373b70e8
Lindemberg <lindembergparaiba@gmail.com>
2024-10-08 19:48:44 -0300

    configurando o primeiro controle

--- src/controllers/CreateCustomerController.ts
+++ src/controllers/CreateCustomerController.ts
@@ -1,3 +1,16 @@
-class CreateCustomerController {}
+import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateCustomerServices } from "../services/CreateCustomerServices";
+
+class CreateCustomerController {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    //inicializo o serviço //
+    const customerService = new CreateCustomerServices();
+
+    // chamo o serviço//
+    const customer = customerService.execute();
+    // devolve para a api //
+    reply.send(customer);
+  }
+}
 
 export { CreateCustomerController };
