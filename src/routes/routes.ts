d20c7315a4aee81e973dc215e95392ddeb245b88
Lindemberg <lindembergparaiba@gmail.com>
2024-10-09 21:39:39 -0300

    feat: rota de listagem de usuarios funcionando

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { CreateCustomerController } from "../controllers/CreateCustomerController";
+import { ListCustomerController } from "../controllers/ListCustomerController";
 
 export async function routes(fastify: FastifyInstance) {
   fastify.get(
@@ -12,6 +13,12 @@ export async function routes(fastify: FastifyInstance) {
     "/customer",
     async (request: FastifyRequest, reply: FastifyReply) => {
       return new CreateCustomerController().handle(request, reply);
+    }
+  );
+  fastify.get(
+    "/customers",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new ListCustomerController().handle(request, reply);
     }
   );
 }
