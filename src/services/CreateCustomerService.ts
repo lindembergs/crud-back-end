c06c29cb8baa8a379b2d06186b2860529197bb9a
Lindemberg <lindembergparaiba@gmail.com>
2025-01-01 20:56:40 -0300

    hello 2025 && git push

--- src/services/CreateCustomerService.ts
+++ src/services/CreateCustomerService.ts
@@ -1,19 +1,29 @@
+import { log } from "console";
 import prismaClient from "../prisma";
 import { CreateCustomerProps } from "../types/index";
+
 class CreateCustomerService {
   async execute({ name, image, linkedin, position }: CreateCustomerProps) {
     if (!name || !image || !linkedin || !position) {
       throw new Error("Necessário preencher os campos");
     }
-    const customer = prismaClient.customer.create({
-      data: {
-        name,
-        image,
-        linkedin,
-        position,
-      },
-    });
-    return customer;
+
+    try {
+      const customer = await prismaClient.customer.create({
+        data: {
+          name,
+          image,
+          linkedin,
+          position,
+        },
+      });
+      return customer;
+    } catch (error) {
+      console.error("Erro ao criar o usuário:", error);
+      throw new Error(
+        "Erro ao criar o usuário. Por favor, tente novamente 2025."
+      );
+    }
   }
 }
 
